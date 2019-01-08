c70cf0cae7144e1cd1de9660a5b37648c8e68eb9
dustincheung <dustincnj@gmail.com>
2019-01-07 20:04:32 -0500

    incrementation/gameover/reset DONE

---
+++ ScoreKeeper.js
@@ -0,0 +1,49 @@
+//getting button objects from html doc
+var p1Butt = document.querySelector("#p1");
+var p2Butt = document.querySelector("#p2");
+var resetButt = document.querySelector("#reset");
+var p1Display = document.querySelector("#p1Display");
+var p2Display = document.querySelector("#p2Display");
+
+//vars that dictate if max score is reached
+var gameDone = false;
+var maxScore = 5
+
+//vars that store the score of each player
+var p1score = 0;
+var p2score = 0;
+
+p1Butt.addEventListener("click", function(){
+	if(!gameDone){
+		p1score++;
+		if(p1score === maxScore){
+			p1Display.classList.add("won");
+			gameDone = true;
+		}
+		p1Display.textContent = p1score;
+	}
+});
+
+p2Butt.addEventListener("click", function(){
+	if(!gameDone){
+		p2score++;
+		if(p2score === maxScore){
+			p2Display.classList.add("won");
+			gameDone = true;
+		}
+		p2Display.textContent = p2score;
+	}
+});
+
+resetButt.addEventListener("click", function(){
+	p1score = 0;
+	p2score = 0;
+	p1Display.textContent = p1score;
+	p2Display.textContent = p2score;
+	p1Display.classList.remove("won");
+	p2Display.classList.remove("won");
+	gameDone = false;
+});
+
+
+
